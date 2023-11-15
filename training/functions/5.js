4c64aa4be08bab2039205c505b80825f19412ff8
mesafeerali321 <mesafeerali@gmail.com>
2023-11-15 07:12:59 +0530

    pushing 5.js and 6.js file

---
+++ training/functions/5.js
@@ -0,0 +1,9 @@
+function isMonitor(rollNumber){
+ if(rollNumber == 1){
+  return 'He is a Monitor'
+ }else{
+  return 'He is not a Monitor'
+ }   
+ 
+}
+console.log(isMonitor(1))
---
+++ training/functions/6.js
@@ -0,0 +1,8 @@
+function isLegend(runs){
+  if(runs > 500){
+    return 'Virat Kohli is a legend cricketer'
+  }else{
+    return 'Virat Kohli is not a good cricketer'
+  }
+}
+console.log(isLegend(600))
