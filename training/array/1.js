ef23a10e9ca47b1d98e3bd5eb4379a697b7ed025
mesafeerali321 <mesafeerali@gmail.com>
2023-11-27 07:54:54 +0530

    adding two array file

---
+++ training/array/1.js
@@ -0,0 +1,24 @@
+let arr = [2, 4, 5, 3]
+
+// print the following
+// 2
+// 4
+// 5
+// 3
+
+// arr.map((num) => {
+//     console.log(num) 
+// })
+
+arr.map(num => console.log(num))
+
+// arr.map(function(num){
+//     console.log(num)
+// })
+
+// arr.map(func)
+
+// function func(num){
+//     console.log(num)
+// }
+   
---
+++ training/array/2.js
@@ -0,0 +1,13 @@
+let arr = [5, 7, 3, 2, 10]
+
+// print the following result
+
+// 10
+// 14
+// 6
+// 4
+// 20
+
+arr.map(function(multiply) {
+    console.log(multiply * 2)
+})
